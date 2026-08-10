2b0d5c54cdcda6b33b13b86fc705862ab57c1d44
Dylan Mordaunt (ISLHD) <Dylan.Mordaunt@health.nsw.gov.au>
2026-08-10 19:11:08 +1000

    fix(tests): serialize current-directory resolver cases

--- Greg.Xrm.Command/Greg.Xrm.Command.Core.TestSuite/Commands/WebResources/PushLogic/FolderResolverTest.cs
+++ Greg.Xrm.Command/Greg.Xrm.Command.Core.TestSuite/Commands/WebResources/PushLogic/FolderResolverTest.cs
@@ -3,6 +3,7 @@ using Microsoft.Xrm.Sdk;
 namespace Greg.Xrm.Command.Commands.WebResources.PushLogic
 {
 	[TestClass]
+	[DoNotParallelize]
 	public class FolderResolverTest
 	{
 #pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider adding the 'required' modifier or declaring as nullable.
